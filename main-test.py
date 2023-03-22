2351f34bf5bffad1e8666ee2c6f792e1c49d8903
Laclac <ngochths150417@fpt.eedu.vn>
2023-03-22 21:23:48 +0700

    test

--- main-test.py
+++ main-test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Ngocht9 - Green version - v2!</p>"
+    return "<p> Hello World - version - ID</p>"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
